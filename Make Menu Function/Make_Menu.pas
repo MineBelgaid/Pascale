eaff7d3420c7cceec26da10a057f19171dfe182b
Mine Belgaid <amin-belgaid@hotmail.com>
2020-11-06 18:14:28 +0100

    Make Menu Function

--- Make Menu Function/Make_Menu.pas
+++ Make Menu Function/Make_Menu.pas
@@ -8,8 +8,9 @@ Function Make_Menu(Options : Array Of String;Exit: boolean): integer;
 Var 
   y : integer;
   Keypressed : char;
-  MaxOptions : integer;
+  MaxOptions,MaxNumbers : integer;
 Begin
+  clrscr;
   textcolor(white);
   writeln('Menu:');
   For y := 0 To high(Options)  Do
@@ -40,52 +41,86 @@ Begin
     gotoxy(4,y+2);
     textcolor(white);
     Keypressed := readkey;
-    Case Keypressed Of 
-      #72 :
-            Begin
-              clreol;
-              If exit And (y = MaxOptions) Then
-                Begin
-                  write('[', y , '] ','Exit The Program.');
-                  y := MaxOptions - 2 ;
-                End
-              Else
-                Begin
-                  write('[', y + 1, '] ', Options[y]);
-                  y := y - 1;
-                  If (y < 0) Then
-                    y := MaxOptions
-                  Else If (y = MaxOptions ) And exit  Then
-                         y := MaxOptions - 2 ;
-                End;
-            End;
-      #80 :
-            Begin
-              clreol;
-              If exit And (y = MaxOptions) Then
-                Begin
-                  write('[', y , '] ','Exit The Program.');
-                  y := 0;
-                End
-              Else
-                Begin
-                  write('[', y + 1, '] ',Options[y]);
-                  y := y + 1;
-                  If y > MaxOptions + 2  Then
-                    y :=  0
-                  Else If exit And (y = MaxOptions - 1 ) Then
-                         y := MaxOptions ;
-                End;
+    If exit Then
+      MaxNumbers := MaxOptions + 48
+    Else
+      MaxNumbers := MaxOptions + 49;
+    If (ord(Keypressed) <= MaxNumbers ) And (Keypressed > #48) Then
+      Begin
+        If exit And (ord(Keypressed) = MaxNumbers) Then
+          Begin
+            If exit And (y  = MaxOptions) Then
+              Begin
+                write('[', y , '] ','Exit The Program. ');
+              End
+            Else
+              Begin
+                write('[', y + 1, '] ',Options[y]);
+                y := MaxOptions;
+              End;
+          End
+        Else
+          Begin
+            If exit And (y  = MaxOptions) Then
+              Begin
+                write('[', y , '] ','Exit The Program. ');
+              End
+            Else
+              write('[', y + 1, '] ',Options[y]);
+            y := ord(Keypressed) - 49 ;
+            gotoxy(10,10);
+            writeln(y);
+            writeln(MaxOptions);
+            writeln(ord(Keypressed));
+          End;
+      End
+    Else
+      Case Keypressed Of 
+        #72 :
+              Begin
+                clreol;
+                If exit And (y = MaxOptions) Then
+                  Begin
+                    write('[', y , '] ','Exit The Program.');
+                    y := MaxOptions - 2 ;
+                  End
+                Else
+                  Begin
+                    write('[', y + 1, '] ', Options[y]);
+                    y := y - 1;
+                    If (y < 0) Then
+                      y := MaxOptions
+                    Else If (y = MaxOptions ) And exit  Then
+                           y := MaxOptions - 2 ;
+                  End;
+              End;
+        #80 :
+              Begin
+                clreol;
+                If exit And (y = MaxOptions) Then
+                  Begin
+                    write('[', y , '] ','Exit The Program.');
+                    y := 0;
+                  End
+                Else
+                  Begin
+                    write('[', y + 1, '] ',Options[y]);
+                    y := y + 1;
+                    If y > MaxOptions   Then
+                      y :=  0
+                    Else If (y > MaxOptions + 2) And exit  Then
+                           y :=  0
+                    Else If exit And (y = MaxOptions - 1 ) Then
+                           y := MaxOptions ;
+                  End;
+              End;
+      End;
 
-
-            End;
-    End;
-
-  Until (Keypressed = #27) or (Keypressed = #13) ;
-   if exit and (y = MaxOptions) then 
-   Make_Menu := -1
-   else 
-   Make_Menu := y;
+  Until (Keypressed = #27) Or (Keypressed = #13) ;
+  If exit And (y = MaxOptions) Then
+    Make_Menu := -1
+  Else
+    Make_Menu := y + 1 ;
 
 
 End;
